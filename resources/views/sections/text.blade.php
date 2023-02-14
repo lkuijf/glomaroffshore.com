31ba44a5d8c8bf25b8a1d48eece61cbabe5a592e
Leon Kuijf <leon.kuijf@gmail.com>
2023-02-14 10:52:51 +0100

    edit

--- resources/views/sections/text.blade.php
+++ resources/views/sections/text.blade.php
@@ -1,21 +1,43 @@
 <div class="introTextContent">
     <div class="inner">
 
-        {{-- @for ($x=0;$x<2;$x++) --}}
         <div class="introTextWrap">
-            <img src="{{ asset('statics/slideshow/Glomar-Avior.jpg') }}" alt="">
+            @if($imageUrl)<img src="{!! $imageUrl !!}" alt="{{ $imageAlt }}">@endif
             <div>
                 <h2>{!! $header !!}</h2>
                 <p>{!! $text !!}</p>
             </div>
+            @if($btn_orange_url && $btn_orange_text)
             <div>
                 <a href="{!! $btn_orange_url !!}" class="btnOrange">{{ $btn_orange_text }}</a>
             </div>
+            @endif
+            @if($btn_transparent_url && $btn_transparent_text)
             <div>
-                <a href="#" class="btnTransparent">[p] +31 223 525 030</a>
+                <a href="{!! $btn_transparent_url !!}" class="btnTransparent">{{ $btn_transparent_text }}</a>
             </div>
+            @endif
         </div>
-        {{-- @endfor --}}
+
+        @if ($header_2 && $text_2)
+        <div class="introTextWrap">
+            @if($imageUrl_2)<img src="{!! $imageUrl_2 !!}" alt="{{ $imageAlt_2 }}">@endif
+            <div>
+                <h2>{!! $header_2 !!}</h2>
+                <p>{!! $text_2 !!}</p>
+            </div>
+            @if($btn_orange_url_2 && $btn_orange_text_2)
+            <div>
+                <a href="{!! $btn_orange_url_2 !!}" class="btnOrange">{{ $btn_orange_text_2 }}</a>
+            </div>
+            @endif
+            @if($btn_transparent_url_2 && $btn_transparent_text_2)
+            <div>
+                <a href="{!! $btn_transparent_url_2 !!}" class="btnTransparent">{{ $btn_transparent_text_2 }}</a>
+            </div>
+            @endif
+        </div>
+        @endif
 
     </div>
 </div>
