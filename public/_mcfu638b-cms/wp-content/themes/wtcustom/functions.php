ba9511895abf5b51fa12df75cf6ec4a3d2e8055e
Leon Kuijf <leon.kuijf@gmail.com>
2023-02-23 13:22:12 +0100

    3 lines to translate

--- public/_mcfu638b-cms/wp-content/themes/wtcustom/functions.php
+++ public/_mcfu638b-cms/wp-content/themes/wtcustom/functions.php
@@ -10,7 +10,7 @@ add_action( 'after_setup_theme', 'my_theme_load_theme_textdomain' );
 
 add_action( 'carbon_fields_register_fields', 'crb_attach_theme_options' );
 function crb_attach_theme_options() {
-    Container::make( 'theme_options', __( 'Something to translate', 'wt-custom-lang' ) . ' * ' . __( 'tl2' ) )
+    Container::make( 'theme_options', __( 'Something to translate', 'wt-custom-lang' ) . ' * ' . __( 'tl2' ) . ' * ' . __( 'Theme' ) )
         ->add_fields( array(
             Field::make( 'text', 'crb_text', 'Text Field' ),
         ) );
