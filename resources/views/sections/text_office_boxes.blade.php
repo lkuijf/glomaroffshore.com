481082a17abf86f3ce1dc4133a390d56d1a022f8
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-05 16:03:32 +0200

    more tel: editted

--- resources/views/sections/text_office_boxes.blade.php
+++ resources/views/sections/text_office_boxes.blade.php
@@ -5,7 +5,7 @@
                     {{ $address_line_2 }}<br />
                     {{ $address_line_3 }}<br />
                     {{ $address_line_4 }}</p>
-                <p class="contactIcon phone"><a href="tel:{{ $phone }}">{{ $phone }}</a></p>
+                <p class="contactIcon phone"><a href="tel:{{ Str::replace(' ', '', $phone) }}">{{ $phone }}</a></p>
                 <p class="contactIcon email"><a href="mailto:{{ $email }}">{{ $email }}</a></p>
             </div>
             <div class="iframeWrap"><iframe width="100%" height="400" src="https://maps.google.com/maps?q={{ urlencode($gm_address) }}&t=&z=10&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div>
